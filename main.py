ccf7ce289c7338dd1ad23e9aac6dc4bf8ee084d1
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2022-11-10 20:12:56 +0000

    sum in args

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-def my_sum(*args):
-    print(args)
+# def my_sum(*args):
+#     # print(args)
   
-print(my_sum(3, 4, 5))
+# print(my_sum(3, 4, 5))
 
-#args are for entering as many arguments as possible 
+# #args are for entering as many arguments as possible 
+# def a_sum(*args):
+#   for num in args:
+#     print(num)
+
+# print(a_sum(1))
+# print(a_sum(1, 3))
+# print(a_sum(1,3 ,4, 5, 6))
+
+list = [1,2,3,4,5,6,7,8,9,10]
+def sum_numbers(list):
+  # sum = 0
+  # for num in list:
+  #   sum += num 
+  # return sum
+  return sum(list)
+
+print(sum_numbers(list))
+
+#args 
 def a_sum(*args):
-  for num in args:
-    print(num)
+  # total = 0
+  # for num in args:
+  #   total += num
+  # return total
+  return sum(args)
 
-print(a_sum(1))
-print(a_sum(1, 3))
-print(a_sum(1,3 ,4, 5, 6))
+print(a_sum(1, 2, 3, 4))
+    
